ed3e8e2b17c02aa2ce73986bd887b88ce4b555d7
xiaqijian <2817062995@qq.com>
2018-09-26 14:43:15 +0800

    添加数据循环

--- src/components/head.js
+++ src/components/head.js
@@ -3,21 +3,34 @@ import axios from '../utils/aixos'
 
 
 class Head extends Component {
+  constructor (props) {
+      super(props)
+      this.state = { data: ['222222222222'] }
+  }
   componentDidMount () {
+      console.log(this.state.data)
+      const that = this
      axios.get('/news?lastCursor=1537930137000&pageSize=10')
      .then((res) => {
          console.log(res.data.data)
+         that.setState({
+             data: res.data.data
+         })
+         console.log(that.state.data)
      })
      .catch((err) => {
          console.log(err)
      })
   }
   render () {
+      const _data = this.state.data;
       return (
         <div className="head-class">
-           <h1>
-              顶顶顶顶
-           </h1>
+            {
+                _data.map(function(item, i) {
+                    return <h1 key={i}>{item.title}</h1>
+                })
+            }
         </div>
       );
   }
